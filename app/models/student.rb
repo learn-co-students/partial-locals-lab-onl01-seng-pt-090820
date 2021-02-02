19b7fbdc3b6b356e0a8a0f90eefd15627dbc9156
Carlj0666 <carlj1881@yahoo.com>
2021-02-02 14:03:26 -0800

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -17,7 +17,7 @@ class Student < ActiveRecord::Base
   def self.search(query)
     if query.present?
       where('NAME like ?', "%query%")
-      student.name
+      self.name
     else
       self.all
     end
